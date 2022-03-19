688c63b3e1c15aca98ba99b6cb8e9c9eebab6d6f
arzwa <arzwa@psb.vib-ugent.be>
2022-03-19 22:11:58 +0100

    example

---
+++ example.jl
@@ -0,0 +1,22 @@
+using MSCSimPlot
+
+S = nw"(((A:20,B:20):10,C:30):5,(D:25,E:25):10);"
+N = Dict(id(n)=>rand(10:30) for n in postwalk(S))
+S.data.distance=20  # root branch
+
+d = MSCSimPlot.msc_layout(S, N)
+P, X = MSCSimPlot.get_ancestry(S, d)
+
+p = plot(legend=false, size=(1200,900), grid=false, framestyle=:none)
+for x in P
+    plot!(x, marker=4, color=:lightgray, markerstrokecolor=:lightgray)
+end
+for l in getleaves(S)
+    xs, ys = d[id(l)]
+    x0s = sample(xs, 3, replace=false)
+    for x0 in x0s
+        plot!(MSCSimPlot.traceback(X, (x0,0)), color=:black, lw=2, alpha=0.3)
+    end
+end
+plot(p)
+
---
+++ src/MSCSimPlot.jl
@@ -0,0 +1,82 @@
+module MSCSimPlot
+
+# Simulate a WF population along a species tree for an MSC model
+using NewickTree, Plots, StatsBase, Distributions
+
+# Get a layout 
+function msc_layout(tree, Ns, pad=5)
+    xleaf = 0
+    coord = Dict()
+    function walk(n)
+        if isleaf(n)
+            N = Ns[id(n)]
+            t = Int(distance(n))
+            coord[id(n)] = (xleaf:(xleaf+N), 0:t-1) 
+            xleaf += N+pad
+        else
+            x1, y1 = walk(n[1])
+            x2, y2 = walk(n[2])
+            N = Ns[id(n)]
+            t = Int(distance(n))
+            x = x1[end] - (N÷2)
+            y = y1[end] + 1
+            coord[id(n)] = (x:(x+N), y:(y+t-1))
+        end
+        return coord[id(n)]
+    end
+    walk(tree)
+    return coord
+end
+
+function get_ancestry(tree, coords)
+    allpaths  = Vector{Tuple{Int,Int}}[]
+    fullpaths = Dict()
+    function walk(n, t, currpaths)
+        xs, ys = coords[id(n)]
+        if isroot(n) && t == 1
+            y = ys[end]
+            currpaths = [[(x,y)] for x=xs]
+            walk(n, t+1, currpaths)
+        elseif t == distance(n) + 1
+            if !isleaf(n)
+                walk(n[1], 1, copy(currpaths))
+                walk(n[2], 1, copy(currpaths))
+            else
+                push!(allpaths, currpaths...)
+            end
+        else
+            noff = rand(Multinomial(length(xs), length(currpaths)))
+            xoff = xs[1]
+            newpths = []
+            for (pth, m) in zip(currpaths, noff)
+                xp, yp = pth[end]
+                if m == 0 
+                    push!(allpaths, pth)
+                else
+                    # prolong the path
+                    push!(allpaths, pth)
+                    for j=1:m
+                        xy = (xoff, yp-1)
+                        push!(newpths, [pth[end] ; xy])
+                        fullpaths[xy] = [copy(pth) ; (xoff, yp-1)]
+                        xoff += 1
+                    end
+                end
+            end
+            walk(n, t+1, newpths)
+        end
+    end
+    walk(tree, 1, Vector{Tuple{Int,Int}}[])
+    return allpaths, fullpaths
+end
+
+function traceback(paths, x)
+    pth = [x]
+    while haskey(paths, x)
+        push!(pth, reverse(paths[x])...)
+        x = paths[x][1]
+    end
+    return pth
+end
+
+end
